39426dcf1b1698ac5144c2437d9e90c3c224e1b7
Khanh Bui <kbuiblue@gmail.com>
2023-05-13 22:46:10 +0700

    Initial commit

---
+++ src/main/java/com/onlydan/od/OnlydanApplication.java
@@ -0,0 +1,13 @@
+package com.onlydan.od;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlydanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlydanApplication.class, args);
+	}
+
+}

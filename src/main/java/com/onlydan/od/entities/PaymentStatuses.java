9453aa8367842014492b0fcd9aee960c49af90a1
Khanh Bui <kbuiblue@gmail.com>
2023-05-14 11:47:31 +0700

    Created all 13 Entities
    Also capitalized the "d" in Onlydan

---
+++ src/main/java/com/onlydan/od/entities/PaymentStatuses.java
@@ -0,0 +1,19 @@
+package com.onlydan.od.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentStatuses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentStatusId;
+    @Column
+    private String statusName;
+}

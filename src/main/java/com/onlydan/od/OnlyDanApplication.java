18f4df61b42f45a3c96ba412df900730ec26ae47
Khanh Bui <kbuiblue@gmail.com>
2023-05-14 12:05:07 +0700

    Created all 13 Entities (again?)

--- src/main/java/com/onlydan/od/OnlyDanApplication.java
+++ src/main/java/com/onlydan/od/OnlyDanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlyDanApplication {
+public class OnlydanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlyDanApplication.class, args);
+		SpringApplication.run(OnlydanApplication.class, args);
 	}
 
 }

b56d2c7ff92a2a6477be8980afb079e39805eac4
Khanh Bui <kbuiblue@gmail.com>
2023-05-14 12:06:55 +0700

    Revert "Created all 13 Entities (again?)"
    This reverts commit 18f4df61b42f45a3c96ba412df900730ec26ae47.

---
+++ src/main/java/com/onlydan/od/entities/Roles.java
@@ -0,0 +1,23 @@
+package com.onlydan.od.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Column
+    private String roleName;
+
+    @Column
+    private String permissions;
+}
